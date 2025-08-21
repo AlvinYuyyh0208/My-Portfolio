72dd365aefd4fbd6ee7d833197f8707f431d1f1a
Yuanhan Yu <avilnyu@gmail.com>
2025-08-21 15:42:38 -0500

    first web launch-remove unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import portrait from '../public/portrait.jpg'
 import { FaGithub, FaLinkedin, FaInstagram, FaCoffee } from 'react-icons/fa';
